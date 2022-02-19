d60118c1a621ee8e61f17998895453114b486458
sobaint01 <walebellomail@gmail.com>
2022-02-19 03:38:02 -0500

    update

--- js/index.js
+++ js/index.js
@@ -14,8 +14,13 @@ $("#contact").hover( function(){
   $(".navChildcontact").slideToggle();
 });
 
-$("#searchIcon").hover( function(){
+$("#searchIcon").hover ( function(){
   $(".searchform").css("display","flex" );
+  
+});
+
+$("#formCloser").onClick ( function(){
+  $("searchform").css("display","none" );
   
 });
 
